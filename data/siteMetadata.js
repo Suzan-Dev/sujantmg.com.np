86d6cc8853a19c70d3d4983f7404033db12fb017
Sujan Tamang <everestgaming3@gmail.com>
2021-08-20 15:39:13 +0545

    Update footer, about page & blog image

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -52,7 +52,7 @@ const siteMetadata = {
       // https://utteranc.es/
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
       issueTerm: 'pathname', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
+      label: 'comment', // label (optional): Comment 💬
       // theme example: github-light, github-dark, preferred-color-scheme
       // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
       theme: 'github-light',
