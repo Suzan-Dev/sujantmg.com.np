0522ba8824b8cf1af8a1d15a9d342b1cd3094571
Sujan Tamang <everestgaming3@gmail.com>
2021-08-20 11:05:40 +0545

    Update footer & project data

--- components/Footer.js
+++ components/Footer.js
@@ -22,8 +22,8 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="https://github.com/Suzan-Dev/sujantmg.com.np">
+            Contribute to this on Github
           </Link>
         </div>
       </div>
--- data/projectsData.js
+++ data/projectsData.js
@@ -4,7 +4,7 @@ const projectsData = [
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/pokocart.png',
     href: 'https://www.google.com',
   },
   {
@@ -12,7 +12,7 @@ const projectsData = [
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/ezbazaar.png',
     href: '/blog/the-time-machine',
   },
 ]
