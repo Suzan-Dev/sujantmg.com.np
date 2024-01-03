9d43deafff9450bca98a517636fed779839eb4f0
Sujan Tamang <58097404+Suzan-Dev@users.noreply.github.com>
2024-01-03 19:18:16 +0545

    Update index.js
    add custom event

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { track } from '@vercel/analytics';
 import Link from '@/components/Link'
 import { PageSeo } from '@/components/SEO'
 import Tag from '@/components/Tag'
@@ -19,7 +20,10 @@ export default function Home({ posts }) {
       <PageSeo title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1
+            className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+            onClick={() => track("Latest")}
+          >
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
