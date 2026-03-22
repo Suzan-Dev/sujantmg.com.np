8fb26f28fc1710aca116bc21238fc82cf21346d2
Sujan Tamang <everestgaming3@gmail.com>
2026-03-22 11:54:30 +0545

    chore: 2026 update

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,8 +1,8 @@
 ---
 name: Sujan Tamang
-avatar: /static/images/avatar.jpg
+avatar: /static/images/avatar.jpeg
 occupation: Software Developer
-company: Botsplash
+company:
 email: everestgaming3@gmail.com
 twitter: https://twitter.com/SujanTa88350485
 linkedin: https://www.linkedin.com/in/sujan-tamang-25b640198
@@ -11,26 +11,13 @@ github: https://github.com/Suzan-Dev
 
 ## About me 👋
 
-Hi! I'm **Sujan Tamang**, a self-taught software developer with over 3+ year of experience.
+Hi! I'm **Sujan Tamang**, a self-taught software developer with over 6+ year of experience.
 I have a flair for creating elegant solutions and building better applications for the world.
 
-Passionate about **Web development** & **Open source**.
+Passionate about **Software** & **Open source**.
 **Javascript** being my core technical skill, there's no limit for my interest of learning new technologies.
 
-Started my journey as a **Frontend React.js Developer** to **Backend Node.js Developer**, currently I'm
-working as a **Full Stack Javascript Developer** at [Botsplash](https://www.botsplash.com).
-I've worked on two biggest **E-commerce projects**, [**Test Case Management Software**](https://app.testmink.com)
-and numerous [**side projects**](https://github.com/Suzan-Dev?tab=repositories) of my own.
+Started my journey as a **React.js Developer** in a small startup company in Nepal to working on multiple US based and Australian based companies.
 I love being a part of the tech communities, learn new technologies, solve problems & write clean code.
-
-## What I am learning in 2023 🐱‍💻
-
-- Redis
-- Websockets
-- Elasticsearch
-- Neo4j
-- Turborepo
-- AWS
-- Microservices
 
 I hope you'll enjoy reading the blog. Have a great day!
--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'Lawpath',
+    description: `Lawpath is an online legal service that makes it faster and easier for businesses to access legal solutions solely based on their own preferences.`,
+    imgSrc: '/static/images/projects/lawpath.svg',
+    href: 'https://www.lawpath.com.au',
+  },
+  {
     title: 'Botsplash',
     description: `Botsplash is a modern omnichannel consumer engagement platform which helps you to engage with your customers
     where they are and helps to achieve higher sales by making you efficient.`,
@@ -20,13 +26,6 @@ const projectsData = [
     href: 'https://app.testmink.com',
   },
   {
-    title: 'Kee',
-    description: `An UK-based client project where content management is directly built into the website. 
-    For content management, we used TinaCMS an open source editor that brings visual editing into React websites.`,
-    imgSrc: '/static/images/projects/kee.png',
-    href: 'https://uat-kee-new-frontend.ekbana.net',
-  },
-  {
     title: 'PokoCart',
     description: `Another big project where I worked as a MERN Stack Developer to built an e-commerce app,
     a bit similar in functionality to daraz. By working on this project, I learned and knew the architecture of bigger apps.`,
@@ -35,27 +34,11 @@ const projectsData = [
     // href: '/blog/the-time-machine',
   },
   {
-    title: 'EzBazaar',
-    description: `My first client project where I built the frontend part of the e-commerce app,
-    a bit similar in functionality to Instacart. Later while working on this project, I also learned
-    about nodejs & mongodb database.`,
-    imgSrc: '/static/images/projects/ezbazaar.png',
-    href: 'http://ezbazaaronline.com',
-  },
-  {
     title: 'Crwn Clothing',
     description: `My biggest personal e-commerce project which I built for learning react.
     All the data are fetched from Firebase's Firestore database. I used stripe for handling the payments.`,
     imgSrc: '/static/images/projects/crwn-clothing.png',
     href: 'https://crwn-clothing-nep.herokuapp.com',
-  },
-  {
-    title: 'Meal Finder',
-    description: `A small personal project built to learn vanilla javascript along with
-    HTML and CSS. We can search for a meal or get random one and view it's recipe.
-    It gets all of the data from a free hosted meals API.`,
-    imgSrc: '/static/images/projects/meal-finder.png',
-    href: 'https://mealsfinder.netlify.app',
   },
 ]
 
